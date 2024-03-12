577a1230f584bd8588df402b02e08f24d966aba5
nicodev711 <124818423+nicodev711@users.noreply.github.com>
2024-03-12 14:09:19 +0000

    Testing up and running

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,28 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe('Rock Paper Scissors Game', () => {
+  beforeEach(() => {
+    cy.visit('https://nicodev711.github.io/CodeSpace-Testing/');
+  });
+  it('should display the user and computer selection for Rock', () => {
+    cy.get('#rock').click();
+    cy.get('#user-option').should('contain', 'Rock');
+    cy.get('#computer-option').should('not.be.empty');
+  });
+
+  it('should display the user and computer selection for Paper', () => {
+    cy.get('#paper').click();
+    cy.get('#user-option').should('contain', 'Paper');
+    cy.get('#computer-option').should('not.be.empty');
+  });
+
+  it('should display the user and computer selection for Scissors', () => {
+    cy.get('#scissors').click();
+    cy.get('#user-option').should('contain', 'Scissors');
+    cy.get('#computer-option').should('not.be.empty');
+  });
+
+  it('should display the result after the user makes a selection', () => {
+    cy.get('#rock').click();
+    cy.get('#results').should('not.be.empty');
+  });
+
+});
